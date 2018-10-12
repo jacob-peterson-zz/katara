2ff6f45f5aace6e009d8cd1d64c97ce9c32ede47
Matthew Schneider <schnei5@wwu.edu>
2018-10-12 13:14:28 -0700

    inital commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed = 10f;
+    public float rotateSpeed = 5f;
+    public float jumpForce = 6f;
+
+    private Rigidbody rb;
+    public LayerMask groundLayer;
+    public CapsuleCollider col;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        col = GetComponent<CapsuleCollider>();
+        groundLayer = LayerMask.GetMask("Default");
+    }
+
+    void Update()
+    {
+        var moveVert = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        var moveHoriz = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var rotate = Input.GetAxis("Mouse X") * rotateSpeed;
+
+        transform.Rotate(0, rotate, 0);
+        transform.Translate(moveHoriz, 0, moveVert);
+
+        if (IsGrounded() && Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            Debug.Log("Jump button pressed");
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.CheckCapsule(col.bounds.center,
+            new Vector3(col.bounds.center.x, col.bounds.min.y, col.bounds.center.z),
+            col.radius * 0.9f,
+            groundLayer);
+    }
+}

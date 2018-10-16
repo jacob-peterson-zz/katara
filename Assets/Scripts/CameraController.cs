1a6cd1caacf33c86bd8fbaf8d8e6de5b626d7bd5
jacob.peterson <jacob.peterson@faithlife.com>
2018-10-15 22:44:00 -0700

    Added castle wall

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	private void Update () {
+	    var mouseY = (Input.mousePosition.y / Screen.height) - 0.7f;
+	    transform.localRotation =
+	        Quaternion.Euler(new Vector4(-1f * (mouseY * 90f), 0f, transform.localRotation.z));
+	}
+}

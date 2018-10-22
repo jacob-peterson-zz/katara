a51c17323fc0a5bf39aea84fd112d254ff4e0483
jacob.peterson <jacob.peterson@faithlife.com>
2018-10-22 00:03:58 -0700

    Added gate, houses, and statue to town

---
+++ Assets/FrontGateController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrontGateController : MonoBehaviour
+{
+
+    private Animator doorAnim;
+    private bool open = false;
+    
+	void Start ()
+	{
+	    doorAnim = GetComponent<Animator>();
+	}
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            open = !open;
+            doorAnim.SetBool("Open", open);
+        }
+    }
+}
